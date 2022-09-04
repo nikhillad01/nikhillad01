875bf0b8c04e6dbc5907f710ed4658827341432b
Nikhil Lad <nikhillad01@gmail.com>
2022-09-04 10:05:42 +0530

    fix: homepage url link not working

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+const gerHomePagePath=()=>{
+  if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"){
+    return ""
+  }
+  else return "nikhillad01"
+}
+
 function Navbar() {
   return (
     <nav class="relative flex flex-wrap items-center justify-between px-2 py-2">
@@ -7,7 +14,7 @@ function Navbar() {
         <div class="w-full relative flex justify-between lg:w-auto  lg:static lg:block lg:justify-start">
           <a
             class="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap"
-            href="/"
+            href={"/"+gerHomePagePath()}
           >
             nikhil lad
           </a>

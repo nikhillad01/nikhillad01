c98aac889cf550ba19ea41b7986c39b57408617b
Nikhil Lad <nikhillad01@gmail.com>
2024-07-05 14:14:16 +0530

    feat: add medium link

--- portfolio/src/components/aboutMe.jsx
+++ portfolio/src/components/aboutMe.jsx
@@ -7,9 +7,28 @@ function AboutMe() {
       <div className="text-left text-gray-800 lowercase leading-relaxed">
         hi ✋, I'm a full-stack backend developer. I have expertise in creating
         software products and solutions that have an impact and produce
-        measurable business benefits.
-        Currently building products at <a  href="https://cargill.com/" target="_blank"  rel="noreferrer" className="font-medium  dark:text-green-700 hover:underline">cargill</a>
+        measurable business benefits. Currently building products at{" "}
+        <a
+          href="https://cargill.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-medium  dark:text-green-700 hover:underline"
+        >
+          cargill
+        </a>
       </div>
+      <p className="text-left text-gray-800 lowercase leading-relaxed">
+        checkout my{" "}
+        <a
+          href="https://medium.com/@nikhillad01"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline"
+        >
+          Medium articles
+        </a>
+        .
+      </p>
     </>
   );
 }
